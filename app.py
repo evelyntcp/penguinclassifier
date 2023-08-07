bae7d92a1b6c5e22e4f27f8ed315f2b55eb40ca1
Evelyn Tan <130960163+icepenguinz@users.noreply.github.com>
2023-08-07 20:35:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,38 @@ import os
 import torch
 from google_drive_downloader import GoogleDriveDownloader as gdd
 
+# Define CNN model
+class PenguinCNN(nn.Module):
+    def __init__(self, num_classes):
+        super(PenguinCNN, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.BatchNorm2d(128),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(128 * 28 * 28, 512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
 # Load the .pth file
 model = torch.load('quantized_penguin_model.pth', map_location=torch.device('cpu'))
 model.eval()
